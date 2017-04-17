c53357bae4f2fa315409dd1374e1c14f9e90032e
lilinj2000 <lilinj2000@gmail.com>
2017-04-17 09:39:30 +0000

    updated.

--- src/env.sh
+++ src/env.sh
@@ -1,14 +1,12 @@
 
 home_3rd=~/3rd
 
-if test -d /llj/3rd ; then
-   home_3rd=/llj/3rd
-fi
+lib_boost=$home_3rd/boost/lib
 
-home_boost=$home_3rd/boost
-home_rem=$home_3rd/rem_api_1.0.1.21
+lib_rem_trade=$home_3rd/rem_api_1.0.1.28/lib/trade/C++/linux64
+lib_rem_quote=$home_3rd/rem_api_1.0.1.28/lib/quote/C++/linux64
 
-export LD_LIBRARY_PATH=$home_boost/lib:$home_rem/lib/trade/C++/linux64_debug
+export LD_LIBRARY_PATH=$lib_boost/lib:$lib_rem_trade:$lib_rem_quote
 
 
 
