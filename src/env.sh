b50d5434069892f7ed066170a6104ab959bb5c41
lilinj2000 <lilinj2000@gmail.com>
2017-04-17 09:42:18 +0000

    updated.

--- src/env.sh
+++ src/env.sh
@@ -6,7 +6,7 @@ lib_boost=$home_3rd/boost/lib
 lib_rem_trade=$home_3rd/rem_api_1.0.1.28/lib/trade/C++/linux64
 lib_rem_quote=$home_3rd/rem_api_1.0.1.28/lib/quote/C++/linux64
 
-export LD_LIBRARY_PATH=$lib_boost/lib:$lib_rem_trade:$lib_rem_quote
+export LD_LIBRARY_PATH=$lib_boost:$lib_rem_trade:$lib_rem_quote
 
 
 
